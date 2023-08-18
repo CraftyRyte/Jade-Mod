cd40e432ecdb754788d6911be77ee9196fa08994
CraftyRyte <craftyryte2011@gmail.com>
2023-08-18 17:17:46 +0530

    Initial Commit

---
+++ src/main/java/com/craftyryte/jademod/JadeModClient.java
@@ -0,0 +1,10 @@
+package com.craftyryte.jademod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JadeModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/craftyryte/jademod/JadeModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.craftyryte.jademod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class JadeModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
